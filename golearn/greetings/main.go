bd4a452daf9be411317fe049656f0e752e0db01b
WangTong <copper.w@foxmail.com>
2023-07-20 13:52:12 +0800

    learn go module

---
+++ golearn/greetings/main.go
@@ -0,0 +1,42 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// Return a greeting that embeds the name in a message.
+
+	if name == "" {
+		return "", errors.New("no name")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
