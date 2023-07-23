9a2ff4eb1cda7d6a1c59b7ab6b068df4a96df4f7
WangTong <copper.w@foxmail.com>
2023-07-23 12:39:53 +0800

    go test

---
+++ golearn/greetings/greetings_test.go
@@ -0,0 +1,22 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Gladys")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
